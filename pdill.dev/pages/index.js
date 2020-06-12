9ddf3f64a3a7d3832f4a04fdf3a9d5d0cb010da9
Dillweed5446 <paul@pdill.dev>
2020-06-12 11:41:32 -1000

    Fix react-icons import statement

--- pdill.dev/pages/index.js
+++ pdill.dev/pages/index.js
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import { FaStackOverflow, FaWordpress } from 'react-icons'
+import { FaStackOverflow, FaWordpress } from 'react-icons/fa'
 
 
 export default function Home () {
