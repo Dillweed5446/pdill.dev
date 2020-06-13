770bc6f74ef49cc74acbdb0172e89ccb0b0674e9
Dillweed5446 <paul@pdill.dev>
2020-06-13 13:22:10 -1000

    Added height property to boxmodel.  Trying to remove whitespace

--- pdill.dev/components/content-box.js
+++ pdill.dev/components/content-box.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function BoxModel ({children}) {
   return (
     <div style={{
+      height: "100%",
       width: "70%",
       border: "2em primary",
       padding: "10%",
