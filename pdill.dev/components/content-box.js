23a7cf037bf9c7f603d67eecf833a88e1be87d3f
Dillweed5446 <paul@pdill.dev>
2020-06-13 14:06:13 -1000

    Better image crop, update theme with image, insert color hash for boxmodel

--- pdill.dev/components/content-box.js
+++ pdill.dev/components/content-box.js
@@ -5,10 +5,10 @@ export default function BoxModel ({children}) {
   return (
     <div theme={ theme } style={{
       width: '70%',
-      border: '2em primary',
+      border: '2em #20A4F3',
       padding: '0%',
       margin: '0%',
-      backgroundColor: 'background.default',
+      backgroundColor: '#DAD2D8',
       boxSizing: 'content-box'
     }} >
       {children}
--- pdill.dev/pages/_app.js
+++ pdill.dev/pages/_app.js
@@ -27,7 +27,7 @@ export default function MyApp (props) {
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <BoxModel>
+        <BoxModel >
           <CssBaseline />
           <Component {...pageProps} />
         </BoxModel>
