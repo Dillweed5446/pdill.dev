0f50db63ae7688bfc5ea61b5251ce27a17e73d41
Dillweed5446 <paul@pdill.dev>
2020-06-15 18:30:55 -1000

    :art: :construction: Add styles to footer

--- pdill.dev/components/footer.js
+++ pdill.dev/components/footer.js
@@ -3,12 +3,14 @@ import Typography from '@material-ui/core/Typography'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { FaStackOverflow, FaWordpress } from 'react-icons/fa'
+import { withStyles } from '@material-ui/core'
 
 export default function Footer () {
   return (
-    <footer>
-      <Typography variant="h5" color="success">Additional Resources</Typography>
+    <footer className="footer">
+      <Typography className="footer-heading" variant="h5" color="success">Additional Resources</Typography>
       <a
+        className="icon"
         href="https://github.com/Dillweed5446"
         alt="GitHub"
         target="_blank"
@@ -17,6 +19,7 @@ export default function Footer () {
         <GitHubIcon title="GitHub"/>
       </a>
       <a
+        className="icon"
         href="https://www.linkedin.com/in/paul-dill-99b498aa/"
         alt="LinkedIn"
         target="_blank"
@@ -25,6 +28,7 @@ export default function Footer () {
         <LinkedInIcon title="LinkedIn"/>
       </a>
       <a
+        className="icon"
         href="https://stackoverflow.com/users/12745384/pdill5446?tab=profile"
         alt="StackOverflow"
         target="_blank"
@@ -33,6 +37,7 @@ export default function Footer () {
         <FaStackOverflow title="StackOverflow"/>
       </a>
       <a
+        className="icon"
         href="https://waakaukau.blog/"
         alt="WordPress.com"
         target="_blank"
@@ -43,3 +48,18 @@ export default function Footer () {
     </footer>
   )
 }
+
+  <style jsx>{`
+  .footer {
+    background-color: '#DAD2D8'
+  },
+
+  .footer-heading {
+    padding: '3%',
+
+  },
+  .icon {
+    margin: '3%'
+  }
+
+    `}</style>
