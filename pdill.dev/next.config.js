e0c0fc5f7c311d9d4f6583dcbb758037ab9db2e4
Paul Dill <paul@pdill.dev>
2020-12-05 18:16:36 -1000

    :package: Add progress bar for next build

--- pdill.dev/next.config.js
+++ pdill.dev/next.config.js
@@ -1,2 +1,9 @@
+const withProgressBar = require('next-progressbar')
+
+module.exports = withProgressBar({
+  progressBar: {
+    profile: true
+  }
+})
 const withImages = require('next-images')
 module.exports = withImages()
